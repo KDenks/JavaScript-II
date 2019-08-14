6be33958a70542108f12c75afa24fdd16f160ea2
KDenks <kankoudenkey@gmail.com>
2019-08-13 22:46:49 -0400

    project completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,50 +1,65 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
-
   //Given this problem: 
   
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
   // Potential Solution:
-
-  // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
-
-  // Function invocation 
   firstItem(items, function(first) {
     console.log(first)
   });
-
 */
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let placeHolder = [];
+  for (let i=0; i<list.length; i++){
+    if (item === list[i]){
+      placeHolder.push(list[i]);
+    }
+  }
+  if (!placeHolder[0]){
+    return cb(false);
+  } else {
+    return cb(true);
+  }
 }
+
+// --ES6--
+// function contains(item, list, cb) {
+//   cb(list.includes(item));
+// }
+
+contains('Pencil', items, (cb) => (console.log(cb)));
 
 /* STRETCH PROBLEM */
 
